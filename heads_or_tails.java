aa1269c91b19ba4f50bb3e2c101c26144a026e2c
Yudai5 <s1310116@u-aizu.ac.jp>
2024-07-03 23:55:15 +0900

    Final revision

--- heads_or_tails.java
+++ heads_or_tails.java
@@ -22,8 +22,8 @@ class cointoss{
     } 
     System.out.println("Heads: "+heads+", Tails: "+tails);
     if(heads>tails)
-        System.out.println("You won");
+        System.out.println(name+" won");
     else
-        System.out.println("You lost");
+        System.out.println(name+" lost");
   }
 }
